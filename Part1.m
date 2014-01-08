34f07eaf38b999c58a0c85a33526403d2adbf4e4
Robin Ricard <ricard.robin@gmail.com>
2014-01-08 17:19:24 +0100

    Repli spectral

--- Part1.m
+++ Part1.m
@@ -13,7 +13,6 @@ dt = 3;
 [ Te, fe ] = echToFq(N, a, b);
 
 four = floor(N*0.4/(b - a));
-
 i = 1;
 top = true;
 
@@ -59,25 +58,25 @@ Te
 fe
 
 %% Transformées
-% 
-% 'Affichez ces fonctions ainsi que leurs spectres'
-% 
-% multiplot('Constante x0', x0, N, a, b)
-% 
-% multiplot('Cosinus Périodiquex1', x1, N, a, b)
-% 
-% multiplot('Sinus Périodique x2', x2, N, a, b)
-% 'En utilisant un nombre non entier de périodes entre a et b (fq=3.0045 par ex.), les diracs sont remplacés par des sinus cardinaux'
-% 
-% multiplot('Dirac x3', x3, N, a, b)
-% 
-% multiplot('Exponentielle x4', x4, N, a, b)
-% 
-% multiplot('Rectangle x5', x5, N, a, b)
-% 
-% multiplot('Rectangle Périodique x5p', x5p, N, a, b)
-% 
-% multiplot('Gaussienne x6', x6, N, a, b)
+
+'Affichez ces fonctions ainsi que leurs spectres'
+
+multiplot('Constante x0', x0, N, a, b)
+
+multiplot('Cosinus Périodiquex1', x1, N, a, b)
+
+multiplot('Sinus Périodique x2', x2, N, a, b)
+'En utilisant un nombre non entier de périodes entre a et b (fq=3.0045 par ex.), les diracs sont remplacés par des sinus cardinaux'
+
+multiplot('Dirac x3', x3, N, a, b)
+
+multiplot('Exponentielle x4', x4, N, a, b)
+
+multiplot('Rectangle x5', x5, N, a, b)
+
+multiplot('Rectangle Périodique x5p', x5p, N, a, b)
+
+multiplot('Gaussienne x6', x6, N, a, b)
 
 %% Echantillonage et aliasing
 
@@ -91,3 +90,11 @@ title('Non Aliasée g50')
 figure(2)
 plotFreq(imag(tfour(g120)), N, a, b)
 title('Aliasée g120')
+'Repliement de spectre'
+'Non respect du théorème de shannon fe(=163) > 2*fMax(=120)'
+
+gsin = gFunction(156.34, N, a, b);
+figure(3)
+plotFreq(imag(tfour(gsin)), N, a, b)
+title('Aliasée gsin')
+'On exagère le repliement spectral pour rapprocher les diracs et obtenir un sinus simple'
